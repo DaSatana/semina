e14917d81202e8ef6887490297302296a6d57ba7
Bog <dasatana@ya.ru>
2023-09-18 21:40:42 +0300

    add task

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"{number1} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} не является квадратом {number2}");
+}
